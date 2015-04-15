575dfdd1ab2928ee647b69dff6c1f6d30fcd301b
Jon Edwards <jkedwards@me.com>
2015-04-15 11:39:18 -0700

    add task creation functionality

--- client/index.js
+++ client/index.js
@@ -1,7 +1,45 @@
 'use strict';
+/* global Firebase: true */
 
 $(document).ready(init);
 
+var root, user, tasks;
+
 function init(){
-  
+  $('#set-name').click(setName);
+  root = new Firebase('https://todo-kolohelios.firebaseio.com/');
+  user = root.child('user');
+  tasks = root.child('tasks');
+  user.on('value', userChanged);
+  $('#create-task').click(createTask);
+
+}
+
+function setName(){
+  var name = $('#name').val();
+  user.set(name);
+  $('#name').val('');
+}
+
+function userChanged(snapshot){
+  var name = snapshot.val();
+  $('#header').text('Todo : ' + name);
+}
+
+function createTask(){
+  var title = $('#title').val();
+  var dueDate = $('#due-date').val();
+  dueDate = new Date(dueDate);
+  dueDate = dueDate.getTime();
+  var priority = $('#priority').val();
+  var isComplete = false;
+  var createdAt = Firebase.ServerValue.TIMESTAMP;
+  var task = {
+    title: title,
+    dueDate: dueDate,
+    priority: priority,
+    isComplete: isComplete,
+    createdAt: createdAt
+  };
+  tasks.push(task);
 }
